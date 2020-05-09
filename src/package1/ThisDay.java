2a050085a838fb5ddb8e6877d92e8be958ad125a
Ndrsh <n0966910363@gmail.com>
2020-05-09 14:53:37 +0300

    Change date

--- src/package1/ThisDay.java
+++ src/package1/ThisDay.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class ThisDay {
 
 	public static void main(String[] args) {
-		System.out.println(new Date());
-
+		System.out.println(new Date() + " This day");
+		System.out.println((new Date()).getDate());
 	}
 
 }
